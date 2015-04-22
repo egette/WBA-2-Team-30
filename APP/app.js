b7cacb1283e380cc999871dffba2d3639b7e81cf
egette <enrico.gette@gmail.com>
2015-04-22 14:08:06 +0200

    Aufgabe 2 Lösung
    Jetzt auch in Bunt; #Raverparty

--- APP/app.js
+++ APP/app.js
@@ -1,5 +1,6 @@
 var fs = require('fs');
 var http = require('http');
+var chalk = require("chalk");
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
@@ -16,10 +17,10 @@ fs.readFile("daten/wolkenkratzer.json", function(err, data) {
   var obj = JSON.parse(data.toString()); 
   
   for (i = 0; i < obj.wolkenkratzer.length; i++) { 
-    console.log('Name: '+ obj.wolkenkratzer[i].name);
-	console.log('Stadt: '+ obj.wolkenkratzer[i].stadt);
-	console.log('Hoehe: '+ obj.wolkenkratzer[i].hoehe);
-	console.log('--------------------------------------');
+    console.log(chalk.cyan('Name: '+ obj.wolkenkratzer[i].name));
+	console.log(chalk.green('Stadt: '+ obj.wolkenkratzer[i].stadt));
+	console.log(chalk.red('Hoehe: '+ obj.wolkenkratzer[i].hoehe));
+	console.log(chalk.magenta.bgBlue('--------------------------------------'));
 	
    };
  });
