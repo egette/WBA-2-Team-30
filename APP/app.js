4bb410ae20f44eb6530cc72af8abdbe82ee6b178
egette <enrico.gette@gmail.com>
2015-04-22 14:00:53 +0200

    Aufgabe 1 Lösung
    Funktioniert perfekt!

--- APP/app.js
+++ APP/app.js
@@ -1,10 +1,26 @@
 var fs = require('fs');
 var http = require('http');
 
-var wolkenkrazer = fs.readFile(daten+"/wolkenkratzer.json", function(err, data) { 
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+}).listen(1337, '127.0.0.1');
+console.log('Server running at http://127.0.0.1:1337/');
+console.log('  ');
+console.log('  ');
+
+fs.readFile("daten/wolkenkratzer.json", function(err, data) { 
 
   if (err) throw err;
   
-  console.log(JSON.stringify( data.toString()));
+  var obj = JSON.parse(data.toString()); 
   
+  for (i = 0; i < obj.wolkenkratzer.length; i++) { 
+    console.log('Name: '+ obj.wolkenkratzer[i].name);
+	console.log('Stadt: '+ obj.wolkenkratzer[i].stadt);
+	console.log('Hoehe: '+ obj.wolkenkratzer[i].hoehe);
+	console.log('--------------------------------------');
+	
+   };
  });
+ 
