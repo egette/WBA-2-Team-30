187fa48b2bed4a762c210c190b8f608972c139ad
Bernhard Kelm <bern.kelm@gmail.com>
2015-05-13 14:16:54 +0200

    Express server
    Added server for local on port 3000

---
+++ Aufgabe2/app.js
@@ -0,0 +1,19 @@
+var fs = require('fs');
+var http = require('http');
+var express = require('express');
+var bodyParser = require('body-parser');
+var jsonParser = bodyParser.json();
+
+
+var app = express();
+app.listen(3000);
+
+var env = process.env.NODE_ENV || 'development';
+if ('development' == env) {
+   app.use(express.static(__dirname + '/public'));
+
+	app.use(function(err, req, res, next) {
+		console.error(err.stack);
+		res.end(err.status + ' ' + err.messages);
+	});
+}
