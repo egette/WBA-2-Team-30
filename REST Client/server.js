6c7e03c5c5e2498c380e11ef2ad86affa774d88d
Bernhard Kelm <bern.kelm@gmail.com>
2015-05-27 21:23:35 +0200

    PUT and GET on User
    PUT and GET on user are now working

--- REST Client/server.js
+++ REST Client/server.js
@@ -4,13 +4,11 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var jsonParser = bodyParser.json();
 var redis = require('redis');
-var client = redis.createClient();
-
-var id = 1;
-
-//Starting node Server on localhost:3000
+var db = redis.createClient();
 var app = express();
-app.listen(3000);
+
+app.use(bodyParser.json());
+
 
 var env = process.env.NODE_ENV || 'development';
 if ('development' == env) {
@@ -23,54 +21,71 @@ if ('development' == env) {
 }
 
 //Connecting to redis Server
-client.on('connect', function() {
+db.on('connect', function() {
     console.log('connected to redis');
 });
 
-client.on("error", function (err) {
+db.on("error", function (err) {
     console.log("Error " + err);
 });
 
 
-//Creating User
+
+//Creating a user with ID as string
+app.post('/user', function(req, res) {
+	var newUser = req.body;
+	db.incr('id:user', function(err, rep) {
+		newUser.id = rep;
+		db.set('user:'+newUser.id, JSON.stringify(newUser), function(err, rep) {
+			res.json(newUser);
+		});
+	});
+});
+
+//Getting User by ID
+app.get('/user/:id', function(req, res) {
+	db.get('user:'+req.params.id, function(err, rep) {
+		if(rep) {
+			res.type('json').send(rep);
+		} else {
+			res.status(404).type('text').send('Dieser User ist nicht vorhanden')
+		}
+	});
+});
+
+//Updating information of a User
+app.put('/user/:id', function(req, res) {
+	db.exists('user:'+req.params.id, function(err, rep) {
+		if(rep == 1) {
+			var updatedUser = req.body;
+			updatedUser.id = req.params.id;
+			db.set('user:' + req.params.id, JSON.stringify(updatedUser), function(err, rep) {
+				res.json(updatedUser);
+			});
+		} else {
+			res.status(404).type('text').send('Dieser User ist nicht vorhanden');
+		}
+	});
+});
+
+//Starting node Server on localhost:3000
+app.listen(3000);
+
+/* Alternate function which are archived now
 app.post('/user', jsonParser, function(req, res) {
 	var username = req.body.username;
 	var password = req.body.password;
 	var name = req.body.name;
 	var surname = req.body.surname;
-	client.hmset('user:'+id, 'username', username, 'password', password, 'name', name, 'surname', surname, redis.print);
+	db.hmset('user:', 'username', username, 'password', password, 'name', name, 'surname', surname, redis.print);
 	res.type('plain').send('Added a new User').end();
-	id++;
 });
 
 //Getting a user
 app.get('/user', function(req, res) {
-	client.hget("user:1", "name", function (err, reply) {
-		res.send(reply.toString()).status(200);
-		client.end();
+	db.hgetall('user:1', function (err, reply) {
+		res.send(reply).status(200);
+		db.end();
 		res.end();
 	});
-});
-
-
-//Old Code from Exercise 2
-/*app.get('/', function(req, res) {
-	fs.readFile("daten/fragen.json", function(err, data) { 
-
-  		if (err) throw err;
-  
-  		obj = JSON.parse(data.toString()); 
-  	});
-
-  	res.send(obj).status(200);	
-});
-
-
-app.post('/answer', jsonParser, function(req, res) {
-	answer.push(req.body);
-	res.type('plain').send('Added!').end();
-});
-
-app.get('/answer', function(req, res) {
-	res.status(200).json(answer);
 });*/
