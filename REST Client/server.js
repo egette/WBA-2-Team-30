0498c7b77296797c6d877cc526f5f35a45e818ce
Bernhard Kelm <bern.kelm@gmail.com>
2015-06-04 16:22:09 +0200

    Removed Code
    Removed old, unused code

--- REST Client/server.js
+++ REST Client/server.js
@@ -175,22 +175,3 @@ app.get('/question', function(req, res) {
 
 //Starting node Server on localhost:3000
 app.listen(3000);
-
-/* Alternate function which are archived now
-app.post('/user', jsonParser, function(req, res) {
-	var username = req.body.username;
-	var password = req.body.password;
-	var name = req.body.name;
-	var surname = req.body.surname;
-	db.hmset('user:', 'username', username, 'password', password, 'name', name, 'surname', surname, redis.print);
-	res.type('plain').send('Added a new User').end();
-});
-
-//Getting a user
-app.get('/user', function(req, res) {
-	db.hgetall('user:1', function (err, reply) {
-		res.send(reply).status(200);
-		db.end();
-		res.end();
-	});
-});*/
