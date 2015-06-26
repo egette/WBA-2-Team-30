25c6563c6992aaf48a5bf42ecc6b84cd3629c222
JessLee5 <jess32@gmx.net>
2015-06-26 18:07:02 +0200

    neue Fragen einstellen hinzugefügt
    Dienstnutzer.js kann nun ein get auf /newquestion bearbeiten. Es wird
    eine Seite mit Formular angezeigt, und die Eingabe wird als JSON String
    per POST auf /question gesendet. Die Eingaben werden noch nicht
    gespeichert.

--- HTTP_server/dienstnutzer.js
+++ HTTP_server/dienstnutzer.js
@@ -54,6 +54,31 @@ app.get('/question', jsonParser, function(req, res) {
 	}); 
 });
 
+app.get('/newquestion', jsonParser, function(req, res) {
+
+	fs.readFile('./newquestion.ejs', {encoding: 'utf-8'}, function(err, filestring) {
+		if(err) {
+			throw err;
+		} else {
+
+			console.log('Connected to newquestion');
+				var html = ejs.render(filestring);
+					res.setHeader('content-type', 'text/html');
+					res.writeHead(200);
+					res.write(html);
+					res.end();
+
+			console.log('Request end');
+		}
+	}); 
+});
+
+
+app.post('/question', jsonParser, function(req, res) {
+	var newquestion = req.body;
+	console.log(newquestion);
+});
+
 app.listen(3001, function() {
 	console.log("Server listens on Port 3001");
 });
